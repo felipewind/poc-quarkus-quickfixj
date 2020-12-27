007f46ee62e55f7789ae8cc87d5339ed519942c3
Felipe Henrique Gross Windmoller <felipewind@gmail.com>
2020-12-27 15:19:32 -0300

    finish docker-compose to run project

--- run.sh
+++ run.sh
@@ -3,6 +3,24 @@
 printf "== Executing Docker Compose with Build to start the application ==\n"
 printf "\n"
 
+printf "========== Building local Acceptor Project with local Maven ============\n"
+printf "\n"
+cd poc-fix-acceptor/
+mvn package -Dquarkus.package.type=fast-jar
+cd ..
+printf "\n"
+printf "============ Acceptor Project builded successfully by  Maven ===========\n"
+printf "\n"
+
+printf "========= Building local Initiator Project with local Maven ============\n"
+printf "\n"
+cd poc-fix-initiator/
+mvn package -Dquarkus.package.type=fast-jar
+cd ..
+printf "\n"
+printf "============ Initiator Project builded successfully by  Maven ==========\n"
+printf "\n"
+
 function ctrl_c() {
 printf "\n"
 printf '==========================================================================\n'
